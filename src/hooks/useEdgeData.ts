9fb92e346b9ef63ec4afea4829c74e9bb435bc13
Farzan Shibu <farzanshibu786@gmail.com>
2024-05-26 14:58:12 +0530

    chore: Remove unused useEdgeData hook

--- src/hooks/useEdgeData.ts
+++
@@ -1,9 +0,0 @@
-import { useNodeStore } from '@/store/nodes';
-
-export const useEdgeData = (edgeId: string) => {
-    const { sourceIds, targetIds } = useNodeStore();
-    const sourceId = sourceIds.find((id) => id === edgeId);
-    const targetId = targetIds.find((id) => id === edgeId);
-
-    return { sourceId, targetId };
-};
